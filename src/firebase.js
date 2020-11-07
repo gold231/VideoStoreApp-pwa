5bf9740b14f63968450e92b4545a6353e81701df
Dragon <53452201+gold231@users.noreply.github.com>
2020-11-07 14:22:20 +0700

    firebase

--- src/firebase.js
+++ src/firebase.js
@@ -3,14 +3,14 @@ import "firebase/auth";
 import "firebase/database";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDh94YdF2ZoVz43uNAjtDnKv8i5yTAFQWI",
-  authDomain: "child-video.firebaseapp.com",
-  databaseURL: "https://child-video.firebaseio.com",
-  projectId: "child-video",
-  storageBucket: "child-video.appspot.com",
-  messagingSenderId: "988158307952",
-  appId: "1:988158307952:web:98d701add5266493f44eb5",
-  measurementId: "G-456283PJRR"
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 
 firebase.initializeApp(firebaseConfig);
